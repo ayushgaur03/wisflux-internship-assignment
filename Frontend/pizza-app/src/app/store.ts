16017fd365f206a48e5582b399f400f7317c51df
Ayush Gaur <ayushgaur02@gmail.com>
2022-08-17 16:48:04 +0530

    feat: integrated redux to the Frontend

---
+++ Frontend/pizza-app/src/app/store.ts
@@ -0,0 +1,48 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import rootReducer from "./rootReducer";
+
+// convert object to string and store in localStorage
+function saveToLocalStorage(state: object) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("persistantState", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+// load string from localStarage and convert into an Object
+// invalid output must be undefined
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("persistantState");
+    if (serialisedState === null) return {};
+    console.log(JSON.parse(serialisedState));
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+const preloadedState = {
+  auth: loadFromLocalStorage().auth,
+};
+
+// export type RootState = ReturnType<typeof rootReducer>;
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState,
+});
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
